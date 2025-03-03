28f61ebd6512e983e381813a2b0ab711d22c0b19
arisarisyi <alarisyi@gmail.com>
2025-03-03 21:29:41 +0800

    change jwt strategy

--- src/common/strategy/access-token.strategy.ts
+++ src/common/strategy/access-token.strategy.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+// Fungsi untuk mengekstrak token dari cookie
+const cookieExtractor = (req: any): string | null => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['access_token'];
+  }
+  return token;
+};
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: cookieExtractor,
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_ACCESS_SECRET,
     });
